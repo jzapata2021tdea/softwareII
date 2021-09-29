4785ec7895e6a2eeaa0d37c51f8754938514d866
jzapata2021Tdea <jzapataparra@mail.com>
2021-09-28 22:59:25 -0500

    Actualización archivos interfaces

---
+++ backendcursojava/src/main/java/net/software/backendcursojava/services/userRepository.java
@@ -0,0 +1,11 @@
+package net.software.backendcursojava.services;
+
+import net.software.backendcursojava.entities.UserEntity;
+
+public interface userRepository {
+
+    static UserEntity save(UserEntity userEntity) {
+        return null;
+    }
+
+}
